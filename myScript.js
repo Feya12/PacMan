16081e9476e7143ebc9d544521fd270fd35a8454
Feya12 <feq3344@gmail.com>
2022-11-27 19:58:55 +0200

    added borders

--- myScript.js
+++ myScript.js
@@ -1,82 +1,48 @@
+function move(){
+  if(pacman.offsetLeft||pacman.offsetTop<100){
+    
+  }
+}
 function moveLeft(pacman) {
-    pacman.style.left -= "50px";
+  pacman.style.left = pacman.offsetLeft - 1 + "px";
 }
 function moveRight(pacman) {
-    pacman.style.left += "50px";
+  pacman.style.left = pacman.offsetLeft + 1 + "px";
 }
 
 function moveDown(pacman) {
-    pacman.style.top += "50px";
+  pacman.style.top = pacman.offsetTop + 1 + "px";
 }
 
 function moveUp(pacman) {
-    pacman.style.top -= "50px";
+  pacman.style.top = pacman.offsetTop - 1 + "px";
 }
-let pacman = document.getElementById('pacman');
-function gameLoop(){
-// smenqm w css mejdu image s zatworena i otworena usta, da se smenq na wseki gameloop
-pacman.id='pacman1';
-    document.addEventListener('keydown', function (e) {
+
+
+function gameLoop() {
+  let pacman = document.getElementById('pacman');
+  let currTop = pacman.offsetTop;
+  let currLeft = pacman.offsetLeft;
+
+  // smenqm w css mejdu image s zatworena i otworena usta, da se smenq na wseki gameloop
+  if (id = 'pacman') {
+    pacman.id = 'pacman1';
+  }
+  else {
+    pacman1.id = 'pacman';
+  }
+
+  document.addEventListener('keydown', function (e) {
     switch (e.key) {
-        case 'w': moveUp(pacman); break;
-        case 's': moveDown(pacman); break;
-        case 'a': moveLeft(pacman); break;
-        case 'd': moveRight(pacman); break;
+      case 'w': moveUp(pacman); break;
+      case 's': moveDown(pacman); break;
+      case 'a': moveLeft(pacman); break;
+      case 'd': moveRight(pacman); break;
     }
 
-}
-)
-
-}
-setInterval(gameLoop,90)
-
-
-
-
-
-/*
-ako id e pacman = pacman1;
-ako id pacman1 = pacman;
-  while (e.key === 'W' ) {
-      
-      while (i<100, i++){
-  switch(key){
-      case 'W': moveUp(pacman); break;
-      case 'S': moveDown(pacman); break;
-      case 'A': moveLeft(pacman); break;
-      case 'D': moveRight(pacman); break;
   }
+  )
 
 }
-{
-  if(e.key === 'w')
-  moveUp(pacman)
-})
-document.addEventListener('keydown', function(e){
-  if(e.key === 'a')
-  moveLeft(pacman)
-})
-document.addEventListener('keydown', function(e){
-  if(e.key === 's')
-  moveDown(pacman)
-})
-document.addEventListener('keydown', function(e){
-  if(e.key === 'd')
-  moveRight(pacman)
-})
 
-while (i<100, i++){
-  switch(key){
-      case 'W': moveUp(pacman); break;
-      case 'S': moveDown(pacman); break;
-      case 'A': moveLeft(pacman); break;
-      case 'D': moveRight(pacman); break;
-  }
-}
 
-})
- 
-
-  }
-})
-*/
