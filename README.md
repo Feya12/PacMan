65178fbda3e8f078663856b780ea5347cfc9bc26
Feya12 <ff@auxcode.com>
2022-12-21 21:29:28 +0200

    Steps
    made the steps smaller and the move animation appears  smoother

--- README.md
+++ README.md
@@ -12,11 +12,16 @@ https://soft-madeleine-0e1900.netlify.app
   * So we will leave some animation to be done w/ CSS such as the mouth animaiton movements.
     * We will need a bit of JavaScript, because when PacMan stops, the mouth stops in open state.
 
+<<<<<<< HEAD
 ## 17 December 2022
 
 11:00 CET call ff et dahoum
 
 * Check progress...
+=======
+
+    * Check progress...
+>>>>>>> dc19d4a (Steps)
   * (We learned today the importance to pull before you begin work.)
   * (We demonstrated how one can keep a bulleted list w/ everything one does and this is the report at the end of the day.)
     * (You can dynamically re-order the list.)
@@ -35,3 +40,18 @@ https://soft-madeleine-0e1900.netlify.app
       * To see why without the ifs pacman moves diagonally `interesting`
       * Still the left and top movement are slower than the right and down `HOMEWORK`
     * The PacMan leaves the labyrinth
+<<<<<<< HEAD
+=======
+
+  21 December
+  *TO DO:  
+  1.Still the left and top movement are slower than the right and down;
+  2.The PacMan leaves the labyrinth; How to fix it?;
+  3.Make the move animation appear smoother.
+  4.Heartbeat of each actor?
+  5.Make variables can be used in the whole code. (maybe global variablesma?)
+   - we have the main heartbeat
+  *DONE:
+  1.Made the move animation appear smoother.
+  2.Made steps smaller.
+>>>>>>> dc19d4a (Steps)
--- pacman.js
+++ pacman.js
@@ -1,81 +1,50 @@
-function move(x, y) {
-
-  // Status Report
+function move(x, y) { 
+  // TODO
+  // This should be probably done only once in the beginning of the game. 
+  var moving = document.getElementById("moving");
+  var currTop = moving.offsetTop;
+  var currLeft = moving.offsetLeft;
+  
+  //Status Report
   document.getElementById("status-x").innerHTML = x;
   document.getElementById("status-y").innerHTML = y;
 
-  // TODO
-  // This should be probably done only once in the beginning of the game.
-  let moving = document.getElementById("moving");
-  let border = document.getElementById("frame");
-  let currTop = moving.offsetTop;
-  let currLeft = moving.offsetLeft;
-  let borderTop = border.offsetHeight;
-  let borderLeft = border.offsetLeft;
-
-  // Status Report
+  //Status Report
   document.getElementById("currTop").innerHTML = currTop;
   document.getElementById("currLeft").innerHTML = currLeft;
 
-  // What is this?
-  // if (moving.offsetTop < 0) { moving.offsetTop = 0; y = 0; }
-  // if (moving.offsetLeft > borderLeft) { moving.offsetLeft = 100; x = 0; }
-  // if (moving.offsetLeft < 0) { moving.offsetLeft = 0; x = 0; }
-  // if (moving.offsetTop > borderTop) { moving.offsetTop = 100; y = 0; }
-
   // Here the motion is controlled
-  //
-  // TODO
-  // w/o the ifs the pacman is moving diagonally.
-  // Find out why.
-  if(x!=0){
+  if(x!=0&&currTop>-100||currTop<110){//x>-10&&x<100--> it means x belongs to [-10;100] interval (new way, according to status report)
     moving.style.top = currTop + x + "px";
   }
-  if(y!=0){
+  //else{moving.style.top = 0;} //try to limit the movement, not the right way
+  if(y!=0&&currLeft>-85||currLeft<120){//[-94;110]
   moving.style.left = currLeft + y + "px";
   }
+  //else{moving.style.left = 0;}
  }
-  /*function borderPacman(x,y){
-
-  move(top1,left1)
-  let moving = document.getElementById("moving");
-  //let border = document.getElementById("frame")
-  let x = 225;
-  let y = 248;
-  if(frame.offsetLeft<moving.offsetLeft){moving.offsetLeft=25}
-  else{move(top1,left1)}
-  if(frame.offsetTop<moving.offsetTop){moving.offsetTop=20}
-  else{move(top1,left1)}
-
-  an attempt to make a function which is cheking if the pacman is out of current coordinates
-  but then choosed to try to do this in move()
-}*/
-
 document.addEventListener("keydown", e => {
 
   // Status Report
   document.getElementById("status-key").innerHTML = e.key;
 
-  // Smaller step makes the move animation appear smoother.
   switch (e.key) {
 
-    case 'w': x = -20; y = 0; break;
+    case 'w': x = -10; y = 0; break;
 
-    case 's': x = 20; y = 0; break;
+    case 's': x = 1; y = 0; break;//if s&d are smaller, can the steps be smoother?
 
-    case 'a': x = 0; y = -20; break;
+    case 'a': x = 0; y = -10; break;
 
-    case 'd': x = 0; y = 20; break;
+    case 'd': x = 0; y = 1; break;
 
   }
-
 })
+
 
 function gameLoop() {
   move(x, y)
-  //borderPacman(x1,y1)
 }
- //let x = 10, y = 10
 
 /*
  *
@@ -83,7 +52,6 @@ function gameLoop() {
  *
  */
 setInterval(gameLoop, 150)
-
 
 
 
