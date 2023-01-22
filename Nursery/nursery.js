f4efd4f3f00e1772f14c5ba65ea00ce4d52ebe6d
Feya12 <ff@auxcode.com>
2023-01-22 22:48:20 +0200

    new directory with some tries
    new task was to make a loop which itterate all the elements in the 2D array

---
+++ Nursery/nursery.js
@@ -0,0 +1,16 @@
+function thruAnArray(){
+    var arr = [
+        //currTop,currLeft
+        [130, 116, ArrowUp, ArrowDown, ArrowRight],
+        [130, 50, ArrowUp, ArrowDown, ArrowLeft],
+        [90, 115, ArrowDown, ArrowRight]
+      ];
+
+    let i = 0;
+
+    while (i < arr.length) {
+        console.log(scores[i]);
+        i++;
+    }
+
+}
