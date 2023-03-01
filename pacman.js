e85d7c41831a2d225f6278fde6fff558d76a9e39
Feya12 <ff@auxcode.com>
2023-03-01 22:51:41 +0200

    made the corridor which has currTop = 230pixels

--- pacman.js
+++ pacman.js
@@ -175,6 +175,69 @@ function gameLoop() {
         "ArrowLeft", "ArrowRight"
       ],
       [180, "ArrowLeft", "ArrowUp"]
+    ],
+
+    [230,
+      [
+        -15, - 14, -13, -12, -11,
+        "ArrowRight", "ArrowUp"
+      ],
+      [
+        -10, -9, -8, -7, -6, 
+        -5, -4, -3, -2, -1,
+        0, 1, 2, 3, 4, 
+        5, 6, 7, 8, 9, 
+        10, 11, 12, 13, 14, 
+        15, 16, 17, 18, 19, 
+        20, 21, 22, 23, 24, 
+        25, 26, 27, 28, 29,
+        30, 31, 32, 33, 34, 
+        35, 36, 37, 38, 39,
+        40, 41, 42, 43, 44,
+        45, 46, 47, 48, 49,
+        50, 51, 52, 53, 54, 
+        55, 56, 57, 58, 59,
+        60, 61, 62, 63, 64,
+        65, 66, 67, 68, 
+        "ArrowLeft", "ArrowRight"
+      ],
+      [
+        69, 70, 71, 72,
+        "ArrowLeft", "ArrowRight", "ArrowUp"
+      ],
+      [
+        73, 74, 75, 76, 77,
+        78, 79, 80, 81, 82,
+        83, 84, 85, 86, 87,
+        88, 89, "ArrowLeft", "ArrowRight"
+      ],
+      [
+        90, 91, 92, 93, 94, 
+        95, "ArrowLeft", "ArrowRight", "ArrowUp"
+      ],
+      [
+        96, 97, 98, 99, 100,
+        101, 102, 103, 104, 105,
+        106, 107, 108, 109, 110,
+        111, 112, 113, 114, 115, 
+        116, 117, 118, 119, 120,
+        121, 122, 123, 124, 125,
+        126, 127, 128, 129, 130,
+        131, 132, 133, 134, 135, 
+        136, 137, 138, 139, 140,
+        141, 142, 143, 144, 145,
+        146, 147, 148, 149, 150,
+        151, 152, 153, 154, 155,
+        156, 157, 158, 159, 160,
+        161, 162, 163, 164, 165, 
+        166, 167, 168, 169, 170, 
+        171, 172, 173, 174, 175,
+        "ArrowLeft", "ArrowRight"
+      ],
+      [
+        176, 177, 178, 179, 180, 
+        "ArrowRight", "ArrowUp"
+      ]
     ]
   ];
 
@@ -195,29 +258,29 @@ function gameLoop() {
 
   // Red Ghost
   var redGhost = document.getElementById('ghostRed');
-  var x1 = Math.floor(Math.random()*30);
-  var y1 = Math.floor(Math.random()*30);
+  var x1 = Math.floor(Math.random()*30 - 10);
+  var y1 = Math.floor(Math.random()*30 - 10);
   redGhost.style.top = x1 + 'px';
   redGhost.style.left = y1 + 'px';
 
   // Pink Ghost
   var pinkGhost = document.getElementById('ghostPink');
-  var x2 = Math.floor(Math.random()*40);
-  var y2 = Math.floor(Math.random()*40);
+  var x2 = Math.floor(Math.random()*40 + 65);
+  var y2 = Math.floor(Math.random()*40 + 65);
   pinkGhost.style.left = y2 + 'px';
   pinkGhost.style.top = x2 + 'px';
 
   // Blue Chost
   var blueGhost = document.getElementById('ghostBlue');
-  var x3 = Math.floor(Math.random()*50);
-  var y3 = Math.floor(Math.random()*50);
+  var x3 = Math.floor(Math.random()*50 - 25);
+  var y3 = Math.floor(Math.random()*50 - 25);
   blueGhost.style.left = y3 + 'px';
   blueGhost.style.top = x3 + 'px';
 
   // Orange Ghost
   var orangeGhost = document.getElementById('ghostOrange');
-  var x4 = Math.floor(Math.random()*65);
-  var y4 = Math.floor(Math.random()*65);
+  var x4 = Math.floor(Math.random()*65 + 15);
+  var y4 = Math.floor(Math.random()*65 + 15);
   orangeGhost.style.left = y4 + 'px';
   orangeGhost.style.top = x4 + 'px';
 
@@ -292,7 +355,7 @@ function gameLoop() {
   }
   
   //Move for the ghosts
-  if((x1 > 230 + 'px')&&((y1 < -15 + 'px')||(y1 > 235 + 'px'))){
+ /* if((x1 > 230 + 'px')&&((y1 < -15 + 'px')||(y1 > 235 + 'px'))){
     window.localStorage.setItem("GhostStop", "true");
     GhostStop = "true";
   }
@@ -307,7 +370,8 @@ function gameLoop() {
   if((x4 > 230 + 'px')&&((y4 < -15 + 'px')||(y4 > 235 + 'px'))){
     window.localStorage.setItem("GhostStop", "true");
     GhostStop = "true";
-  }
+    // saw that it does not work
+  }*/ 
 } // END OF gameLoop()
 
 // Here we only listen for the PressedKey, we do not take any action.
