f39fd45c424865b34c819f3e8e971ffc2d171398
Feya12 <ff@auxcode.com>
2023-03-02 16:55:06 +0200

    sorted the array by arrows&made the corridow with 130px currTop 1. left 2. right 3. up 4. down

--- pacman.js
+++ pacman.js
@@ -62,6 +62,28 @@ function gameLoop() {
   // [130, [down, right,-15,20,all directions], [,20,45, all except down]
 
   var corridor = [
+    [130, 
+      [24, "ArrowUp", "ArrowDown"],
+      [49, 50, "ArrowRight", "ArrowUp", "ArrowDown"],
+      [
+        52, 53, 54, 55, 56, 
+        57, 58, 59, 60, 61, 
+        62, 63, 64, 65, 66,
+        67, 68, 69, 70, 71,
+        72, 73, 74, 75, 76,
+        77, 78, 79, 80, 81,
+        82, 83, 84, 85, 86,
+        87, 88, 89, 90, 91,
+        92, 93, 94, 95, 96,
+        97, 98, 99, 100, 101,
+        102, 103, 104, 105, 106,
+        107, 108, 109, 110, 111,
+        112, 113, 114, "ArrowLeft", "ArrowRight"
+      ],
+      [115, 116, "ArrowLeft", "ArrowUp", "ArrowDown"],
+      [144, "ArrowUp", "ArrowDown"]
+    ], 
+
     [150, 
       [
         -15, -14, -13, -12, -11, 
@@ -70,17 +92,17 @@ function gameLoop() {
         0, 1, 2, 3, 4, 5,
         6, 7, 8, 9, 10, 
         11, 12, 13, 14, 15,
-        16, 17, 18, 19, "ArrowRight", "ArrowLeft"
+        16, 17, 18, 19,"ArrowLeft", "ArrowRight"
       ],
-      [20, "ArrowDown", "ArrowLeft", "ArrowUp", "ArrowRight"],
+      [20, "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"],
       [
         21, 22, 23, 24, 25, 26,
         27, 28, 29, 30, 31, 32,
         33, 34, 35, 36, 37, 38,
-        39, 40, 41, 42, 43, 44, "ArrowRight", "ArrowLeft"
+        39, 40, 41, 42, 43, 44, "ArrowLeft", "ArrowRight"
       ],
       [45, "ArrowLeft", "ArrowRight", "ArrowUp"],
-      [89, 90, "ArrowDown", "ArrowRight"],
+      [89, 90, "ArrowRight", "ArrowDown"],
       [
         91, 92, 93, 94, 
         95, 96, 97, 98, 99, 
@@ -124,7 +146,7 @@ function gameLoop() {
       [0, "ArrowLeft", "ArrowDown"],
       [
         25, 26, 27, 28, 29,
-        "ArrowRight", "ArrowLeft", "ArrowDown"
+        "ArrowLeft", "ArrowRight", "ArrowDown"
       ], 
       [
         30, 31, 32, 33, 34,
@@ -133,41 +155,41 @@ function gameLoop() {
         45, 46, 47, 48, 49,
         50, 51, 52, 53, 54,
         55, 56, 57, 58, 59,
-        "ArrowRight", "ArrowLeft"
+        "ArrowLeft", "ArrowRight"
       ],
       [
         60, 61, 62, 63, 64,
         65, 66, 67, 68, 69,
         70, 71, 72, 73, 74,
-        "ArrowRight", "ArrowLeft", "ArrowUp"
+        "ArrowLeft", "ArrowRight", "ArrowUp"
       ],
       [
         75, 76, 77, 78, 79,
         80, 81, 82, 83, 84, 
-        85, 86, 87, 88, "ArrowRight", "ArrowLeft"
+        85, 86, 87, 88, "ArrowLeft", "ArrowRight"
       ],
       [
         89, 90, 91, 92, 93,
         94, 95, 96, 97, 98,
-        99, "ArrowRight", "ArrowLeft", "ArrowUp"
+        99, "ArrowLeft", "ArrowRight", "ArrowUp"
       ],
       [
         100, 101, 102, 103, 104,
         105, 106, 107, 108, 109,
-        110, "ArrowRight", "ArrowLeft"
+        110, "ArrowLeft", "ArrowRight"
       ],
       [
         111, 112, 113, 114, 115,
         116, 117, 118, 119, 120,
-        "ArrowRight", "ArrowLeft"
+        "ArrowLeft", "ArrowRight"
       ],
       [
         121, 122, 123, 124, 125, 
         126, 127, 128, 129, 130,
         131, 132, 133, 134, 135,
-        136, 137, 138, "ArrowRight", "ArrowLeft"
+        136, 137, 138, "ArrowLeft", "ArrowRight"
       ],
-      [139, 140, "ArrowRight", "ArrowLeft", "ArrowDown"],
+      [139, 140, "ArrowLeft", "ArrowRight", "ArrowDown"],
       [170, "ArrowRight", "ArrowDown"],
       [
         171, 172, 173, 174, 175,
