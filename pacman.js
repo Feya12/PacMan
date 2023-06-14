89ed0e113266f147e2ef295d40339dddc794ff1b
Feya12 <ff@auxcode.com>
2023-06-14 23:54:05 +0300

    everything from today

--- pacman.js
+++ pacman.js
@@ -1,11 +1,10 @@
 function Arrow(PressedKey) {
 
   let moving = document.getElementById("moving");
-  // Indicates if PacMan should move or stop eg. against a wall
-  let PacManStop = window.localStorage.getItem("PacManStop");
+
   let currTop = moving.offsetTop;
   let currLeft = moving.offsetLeft;
-  // let PressedKey = window.localStorage.getItem("PressedKey");
+
 
   // Software Model of the Labyrinth
   // Helps us when we need to stop at a wall and turn at a crossing
@@ -14,170 +13,145 @@ function Arrow(PressedKey) {
     //
     //currTop,currLeft
     //                                                //coordinates to the:
-    [130, 119, "ArrowUp", "ArrowDown", "ArrowLeft"],    //first wall right
-    [130, 50, "ArrowUp", "ArrowDown", "ArrowRight"],   //first wall left
-    [87, 119, "ArrowDown", "ArrowLeft"],               //wall right from the red ghost
-    [49, 87, "ArrowUp", "ArrowLeft", "ArrowRight"],   //wall left from the red ghost
-    [150, 116, "ArrowUp", "ArrowLeft", "ArrowRight"],   //down right
-    [150, 50, "ArrowUp", "ArrowLeft", "ArrowRight"],   //down left
-    [87, 50, "ArrowDown", "ArrowRight"],
-    [107, -15, "ArrowRight"],
-    [107, 185, "ArrowLeft"],
-    [107, 24, "ArrowLeft", "ArrowDown", "ArrowUp"],
-    [107, 144, "ArrowRight", "ArrowDown", "ArrowUp"],
-    [107, 119, "ArrowUp", "ArrowDown", "ArrowUp"],
+    [130, 119, "w", "s", "a"],    //first wall right
+    [130, 50, "w", "s", "d"],   //first wall left
+    [87, 119, "s", "a"],               //wall right from the red ghost
+    [49, 87, "w", "a", "d"],   //wall left from the red ghost
+    [150, 116, "w", "a", "d"],   //down right
+    [150, 50, "w", "a", "d"],   //down left
+    [87, 50, "s", "d"],
+    [107, -15, "d"],
+    [107, 185, "a"],
+    [107, 24, "a", "s", "w"],
+    [107, 144, "d", "s", "w"],
+    [107, 119, "w", "s", "w"],
     //
     // all_directions_allowed 
     //
-    [154, 144, "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"],
-    [154, 24, "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"],
-    //
-    //initial coordinates to be possible to go left
-    //
-    [130, 85, "ArrowLeft", "ArrowRight"],
+    [154, 144, "a", "d", "w", "s"],
+    [154, 24, "a", "d", "w", "s"],
     //
     //endmost right
     //
-    [150, 200, "ArrowLeft", "ArrowDown"],
-    [107, 200, "ArrowLeft"],
-    [230, 180, "ArrowLeft", "ArrowUp"],
-    [5, 185, "ArrowLeft", "ArrowDown"],
+    [150, 200, "a", "s"],
+    [107, 200, "a"],
+    [230, 180, "a", "w"],
+    [5, 185, "a", "s"],
     //
     //endmost left
     //
-    [150, -15, "ArrowRight", "ArrowDown"],
-    [107, -15, "ArrowRight"],
-    [5, -15, "ArrowRight", "ArrowDown"],
+    [150, -15, "d", "s"],
+    [107, -15, "d"],
+    [5, -15, "d", "s"],
     //
     //other walls
     //
-    [60, 70, "ArrowLeft", "ArrowDown"],
-    [60, 95, "ArrowRight", "ArrowDown"],
-    [60, 50, "ArrowRight", "ArrowUp"],
-    [60, 120, "ArrowLeft", "ArrowUp"],
-    [230, -15, "ArrowRight", "ArrowUp"],
-    [155, 70, "ArrowLeft", "ArrowDown"],
-    [155, 90, "ArrowRight", "ArrowDown"],
-    [35, 120, "ArrowRight", "ArrowLeft", "ArrowDown"],
-    [35, 50, "ArrowRight", "ArrowLeft", "ArrowDown"],
-    [35, 135, "ArrowRight", "ArrowLeft"],
-    [84, 95, "ArrowRight", "ArrowLeft", "ArrowUp"],
-    [35, 35, "ArrowRight", "ArrowLeft"],
+    [60, 70, "a", "s"],
+    [60, 95, "d", "s"],
+    [60, 50, "d", "w"],
+    [60, 120, "a", "w"],
+    [230, -15, "d", "w"],
+    [155, 70, "a", "s"],
+    [155, 90, "d", "s"],
+    [35, 120, "d", "a", "s"],
+    [35, 50, "d", "a", "s"],
+    [35, 135, "d", "a"],
+    [84, 95, "d", "a", "w"],
+    [35, 35, "d", "a"],
     //
     //divided by currTop
     //
     //
     //5px
     //
-    [5, -15, "ArrowRight", "ArrowDown"],
-    [5, 24, "ArrowLeft", "ArrowRight", "ArrowDown"],
-    [5, 70, "ArrowLeft", "ArrowDown"],
-    [5, 95, "ArrowRight", "ArrowDown"],
-    [5, 144, "ArrowLeft", "ArrowRight", "ArrowDown"],
-    [5, 180, "ArrowLeft", "ArrowDown"],
+    [5, -15, "d", "s"],
+    [5, 24, "a", "d", "s"],
+    [5, 70, "a", "s"],
+    [5, 95, "d", "s"],
+    [5, 144, "a", "d", "s"],
+    [5, 180, "a", "s"],
     //35px
     //
-    [35, -15, "ArrowRight", "ArrowUp", "ArrowDown"],
-    [35, 70, "ArrowLeft", "ArrowRight", "ArrowUp"],
-    [35, 95, "ArrowLeft", "ArrowRight", "ArrowUp"],
-    [35, 119, "ArrowLeft", "ArrowRight", "ArrowDown"],
-    [35, 180, "ArrowLeft", "ArrowUp", "ArrowDown"],
+    [35, -15, "d", "w", "s"],
+    [35, 70, "a", "d", "w"],
+    [35, 95, "a", "d", "w"],
+    [35, 119, "a", "d", "s"],
+    [35, 180, "a", "w", "s"],
     //60px
     //
-    [60, -15, "ArrowRight", "ArrowUp"],
-    [60, 24, "ArrowLeft", "ArrowUp", "ArrowDown"],
-    [60, 70, "ArrowLeft", "ArrowDown"],
-    [60, 95, "ArrowRight", "ArrowDown"],
-    [60, 119, "ArrowLeft", "ArrowUp"],
-    [60, 130, "ArrowLeft", "ArrowUp"],
-    [60, 144, "ArrowRight", "ArrowUp", "ArrowDown"],
-    [60, 180, "ArrowLeft", "ArrowUp"],
+    [60, -15, "d", "w"],
+    [60, 24, "a", "w", "s"],
+    [60, 70, "a", "s"],
+    [60, 95, "d", "s"],
+    [60, 119, "a", "w"],
+    [60, 130, "a", "w"],
+    [60, 144, "d", "w", "s"],
+    [60, 180, "a", "w"],
     //
     //154px
     //
-    [154, 73, "ArrowLeft", "ArrowDown"],
-    [154, 97, "ArrowRight", "ArrowDown"],
-    [154, 119, "ArrowLeft", "ArrowRight", "ArrowUp"],
-    [154, 180, "ArrowLeft", "ArrowDown"],
+    [154, 73, "a", "s"],
+    [154, 97, "d", "s"],
+    [154, 119, "a", "d", "w"],
+    [154, 180, "a", "s"],
     //
     //180px
     //
-    [180, -15, "ArrowLeft", "ArrowRight"],
-    [180, 3, "ArrowLeft", "ArrowDown"],
-    [180, 24, "ArrowRight", "ArrowUp", "ArrowDown"],
-    [180, 50, "ArrowLeft", "ArrowRight", "ArrowDown"],
-    [180, 97, "ArrowLeft", "ArrowRight", "ArrowUp"],
-    [180, 119, "ArrowLeft", "ArrowRight", "ArrowDown"],
-    [180, 144, "ArrowLeft", "ArrowUp", "ArrowDown"],
-    [180, 167, "ArrowRight", "ArrowDown"],
-    [180, 180, "ArrowLeft", "ArrowUp"],
+    [180, -15, "a", "d"],
+    [180, 3, "a", "s"],
+    [180, 24, "d", "w", "s"],
+    [180, 50, "a", "d", "s"],
+    [180, 97, "a", "d", "w"],
+    [180, 119, "a", "d", "s"],
+    [180, 144, "a", "w", "s"],
+    [180, 167, "d", "s"],
+    [180, 180, "a", "w"],
     //
     //204px
     //
-    [204, -15, "ArrowRight", "ArrowDown"],
-    [204, 3, "ArrowLeft", "ArrowRight", "ArrowUp"],
-    [204, 24, "ArrowLeft", "ArrowUp"],
-    [204, 50, "ArrowRight", "ArrowUp"],
-    [204, 73, "ArrowLeft", "ArrowDown"],
-    [204, 97, "ArrowRight", "ArrowDown"],
-    [204, 119, "ArrowLeft", "ArrowUp"],
-    [204, 144, "ArrowRight", "ArrowUp"],
-    [204, 167, "ArrowLeft", "ArrowRight", "ArrowUp"],
-    [204, 180, "ArrowLeft", "ArrowDown"],
+    [204, -15, "d", "s"],
+    [204, 3, "a", "d", "w"],
+    [204, 24, "a", "w"],
+    [204, 50, "d", "w"],
+    [204, 73, "a", "s"],
+    [204, 97, "d", "s"],
+    [204, 119, "a", "w"],
+    [204, 144, "d", "w"],
+    [204, 167, "a", "d", "w"],
+    [204, 180, "a", "s"],
     //
     //228px
     //
-    [228, -15, "ArrowRight", "ArrowUp"],
-    [228, 70, "ArrowLeft", "ArrowRight", "ArrowUp"],
-    [228, 71, "ArrowLeft", "ArrowRight", "ArrowUp"],
-    [228, 73, "ArrowLeft", "ArrowUp"],
-    [228, 97, "ArrowRight", "ArrowUp"],
-    [228, 180, "ArrowLeft", "ArrowUp"]
+    [228, -15, "d", "w"],
+    [228, 70, "a", "d", "w"],
+    [228, 71, "a", "d", "w"],
+    [228, 73, "a", "w"],
+    [228, 97, "d", "w"],
+    [228, 180, "a", "w"]
   ];
 
+
+
   for (let i = 0; i < crossings_and_allowed_directions.length; i++) {
-
     let element = crossings_and_allowed_directions[i];
-
-    if ((currTop == element[0]) && (currLeft == element[1]) && !element.includes(PressedKey)) {
-
-      console.log('IF=======================')
-      console.log(currTop == element[0])
-      console.log(currLeft == element[1])
-      console.log(!element.includes(PressedKey))
-      console.log(PressedKey)
-
-      console.log(true)
-      console.log(currTop)
-      console.log(currLeft)
-      console.log('ENDIF=======================')
-
-
-      window.localStorage.setItem("PacManStop", "true");
-      PacManStop = "true";
+    if ((currTop == element[0]) && (currLeft == element[1]) && element.includes(PressedKey)) {
+      PacManStop = false
     }
-    else if (currTop == element[0] && (currLeft == element[1])) {
-      console.log('ELSEIF=======================')
+    else if (currTop == element[0] && currLeft == element[1]) {
+      PacManStop = true
+      directionsAllowed = element
+    }
 
-      console.log(false)
-      console.log(currTop)
-      console.log(currLeft)
-      console.log('ELSEIFEND=======================')
+  }
 
-      window.localStorage.setItem("PacManStop", "false");
-      PacManStop = "false";
-      // Move
+  if (PacManStop == false) {
+    if (directionsAllowed.includes(PressedKey)) {
       if (x != 0) {
         moving.style.top = currTop + x + "px";
-
       }
-
-      // Move
       if (y != 0) {
         moving.style.left = currLeft + y + "px";
-
       }
-
     }
   }
 }
@@ -191,7 +165,6 @@ function gameLoop() {
 
   let moving = document.getElementById("moving");
   // Indicates if PacMan should move or stop eg. against a wall
-  let PacManStop = window.localStorage.getItem("PacManStop");
   let currTop = moving.offsetTop;
   let currLeft = moving.offsetLeft;
 
@@ -201,7 +174,7 @@ function gameLoop() {
   document.getElementById("status-y").innerHTML = y;
   document.getElementById("currTop").innerHTML = moving.offsetTop;
   document.getElementById("currLeft").innerHTML = moving.offsetLeft;
-  document.getElementById("PacManStop").innerHTML = PacManStop;
+  document.getElementById("PacManStop").innerHTML = directionsAllowed
 
   //
   //
@@ -213,51 +186,33 @@ function gameLoop() {
   // Brain Decision about change of direction based on pressed key
   switch (PressedKey) {
 
-    case "ArrowLeft":
-      Arrow(PressedKey);
+    case "a":
       x = 0;
       y = -1;
+      Arrow(PressedKey);
       break;//left
 
-    case "ArrowUp":
-      Arrow(PressedKey);
+    case "w":
       x = -1;
       y = 0;
+      Arrow(PressedKey);
       break;//up
 
-    case "ArrowDown":
-      Arrow(PressedKey);
+    case "s":
       x = 1;
       y = 0;
+      Arrow(PressedKey);
       break;//down
 
-    case "ArrowRight":
-      Arrow(PressedKey);
+    case "d":
       x = 0;
       y = 1;
+      Arrow(PressedKey);
       break;//right
 
   }
 
-  // Brain Decision based on a collision w/ a wall
-  /*for (let i = 0; i < all_directions_allowed.length; i++) {
-    let element1 = all_directions_allowed[i];
-    if ((currTop == element1[0]) &&
-      (currLeft == element1[1]) && element1.includes(PressedKey)) {
-        window.localStorage.setItem("PacManStop", "false"); 
-        PacManStop = "false";
-    }
-    else{
-      window.localStorage.setItem("PacManStop", "true");
-      PacManStop = "true";
-      
-  }*/
 
-  //
-  //
-  // EXECUTION OF THE DECISIONS OF THE BRAIN
-  //
-  //
 
   // Here the motion is controlled
 
@@ -297,6 +252,8 @@ function InitialisePacMan() {
   moving.style.left = "85px";
 
 
+  x = 1
+
 }
 
 /*
@@ -309,3 +266,5 @@ function InitialisePacMan() {
 setInterval(gameLoop, 20)
 // This is the step of movement
 let x = 0, y = 0;
+PacManStop = false;
+let directionsAllowed = ["a", "d"]
